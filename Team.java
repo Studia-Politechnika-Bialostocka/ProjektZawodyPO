f2d50416051246f3fa431e5281499f1bb0583f12
me3eh <matt30002000@gmail.com>
2020-05-25 21:49:46 +0200

    Dodanie nowej zmiennej:setsWon, dodanie jej do dwoch metod, gdzie w Lost AMatch ustalamy ile setow ugrali oraz usunalem nie wiadomo po cholere zawarta metode AddTeam w klasie Team xD

--- Team.java
+++ Team.java
@@ -3,28 +3,26 @@ package def;
 public abstract class Team {
 	protected int playerAmount;
 	protected String teamName;
-	protected int points;
-	protected int wins;
-	protected int losses;
+	protected int points=0;
+	protected int wins=0;
+	protected int losses=0;
 	protected double prizesWon;
-	
+	protected int setsWon=0;
 	public int getPlayerAmount() {
 		return playerAmount;
-	}
-	
-	public void addNewTeam() {
-		
 	}
 
 	public void WonAMatch()
 	{
 		wins+=1;
 		points+=3;
+		setsWon+=3;
 	}
 
-	public void LostAMatch()
+	public void LostAMatch(int sets)
 	{
 		losses+=1;
+		setsWon+=sets;
 	}
 
 	public String getTeamName() {

e458e5aea7f0ecc4bd5cfe900bb871e0b5808b3e
Mateusz Białowąs <58574619+mateuszbialowas@users.noreply.github.com>
2020-05-27 18:20:21 +0200

    Dodanie klawiatury i menu I poziom

--- Main.java
+++ Main.java
@@ -1,16 +1,29 @@
 package def;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner klawiatura = new Scanner();
 
 		int menu = 0;
+		int wybór = 0;
 		while(menu == 0){
 			System.out.println("---------------MENU---------------");
 			System.out.println("1. Dodaj turiej.");
 			System.out.println("2. Zakończ program.");
+			wybór = klawiatura.nextInt();
+			switch (wybór){
+				case 1:{
 
+				} break;
+				case 2:{
+					System.exit(0);
+				}break;
+			}
 		}
+
 	}
 	
 }

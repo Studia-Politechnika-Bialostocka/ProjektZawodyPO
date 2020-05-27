33e1e034394ef1e1ace787081a4c258eb52afdb5
WooooCash <58574586+WooooCash@users.noreply.github.com>
2020-05-27 16:35:21 +0200

    refchoices

--- Tournament.java
+++ Tournament.java
@@ -2,6 +2,7 @@ package def;
 
 import java.util.LinkedList;
 
+
 public class Tournament {
 	private String name;
 	private double prizePool;
@@ -11,7 +12,8 @@ public class Tournament {
 	private LinkedList<Referee> referees;
 	private Linked List<AssistantReferee> aReferees;
 	
-	
+	private int refChoice = 0;
+	private int aRefChoice = 0;
 	
 	public Tournament(String name, double prize, LinkedList<Team> teams, LinkedList<Referee> refs, LinkedList<AssistantReferee> arefs) {
 		this.name = name;
@@ -19,14 +21,19 @@ public class Tournament {
 		this.teams = teams;
 		referees = refs;
 		aReferees = arefs;
+
+		rand = new Random();
 	}
 	
 	public LinkedList<Team> roundRobin() {
 
 		for (int i = 0; i < teams.size(); i++) {
 			for (int j = i+1; j < teams.size; j++) {
-				if (teams.get(0) instanceof Volleyball)
+
+				if (teams.get(0) instanceof Volleyball) {
+
 					matches.add(new Match(teams.get(i), teams.get(j), referees.get()));
+				}
 
 			}
 		}

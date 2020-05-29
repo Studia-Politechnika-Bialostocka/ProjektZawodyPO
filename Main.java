056d45971aa09f98debd7c5ddbb507ccb5fd2bdb
me3eh <matt30002000@gmail.com>
2020-05-29 23:52:24 +0200

    Popraweczki

--- Main.java
+++ Main.java
@@ -5,14 +5,13 @@ import java.util.Scanner;
 public class Main {
 	public static Beach beach = new Beach();
 	public static void main(String[] args) {
-<<<<<<< HEAD
 
->>>>>>> 670e6ced3309f5a365b06a89cef69b9f1d32aa8b
 		int menu = 0;
 		int menu2 = 0;
 		int choice = 0;
 		int choice2 = 0;
 		while(true){
+			Scanner keyboard = new Scanner(System.in);
 			System.out.println("---------------MENU---------------");
 			System.out.println("1. Add a tournament.");
 			System.out.println("2. Display list of tournaments.");
@@ -23,7 +22,7 @@ public class Main {
 				case 1:{
 					System.out.println("Enter desired tournament name: ");
 					String nazwa = keyboard.next();
-					//listOfTournaments.add(new Tournament(nazwa), 100,listOfReferees,);
+					beach.addTournament(new Tournament());
 				}break;
 				case 2:{
 					//wyświetla listę turniejów
@@ -165,7 +164,7 @@ public class Main {
 		}
 	}
 
-	private static void goToPlayoffs(){
+	private static void goToPlayoffs(Tournament tournament){
 		Scanner keyboard = new Scanner(System.in);
 		int menu = 0;
 		int choice = 0;
@@ -185,12 +184,15 @@ public class Main {
 				}break;
 				case 2:{
 					//pokaż wyniki meczów
+
 				}break;
 				case 3:{
 					//wyświetlanie wszystkich meczy
+
 				}break;
 				case 4:{
 					//ustaw wynik meczu
+
 				}break;
 				case 5:{
 					menu = 1;
